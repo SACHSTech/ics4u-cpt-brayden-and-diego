b6c6c066a82c3995596455ab8382f1a9af4dccf6
D1eg0Verdin <diego.verdinramirez23@ycdsbk12.ca>
2023-05-29 10:29:37 -0400

    created team class

---
+++ src/cpt/team.java
@@ -0,0 +1,32 @@
+package cpt;
+
+public class team {
+    protected String teamName;
+    protected double ppg22;
+    protected double ppg21;
+    protected double pct22;
+    protected double pct21;
+    protected double assists22;
+    protected double assists21;
+    protected double tpg22;
+    protected double tpg21;
+    protected double trg22;
+    protected double trg21;
+
+    public team(String teamName, double ppg22, double ppg21,double pct22,double pct21, double assists22, double assists21,double tpg22, double tpg21, double trg22, double trg12 ){
+        teamName = this.teamName;
+        ppg22 = this.ppg22;
+        ppg21 = this.ppg21;
+        pct22 = this.pct22;
+        pct21 = this.pct21;
+        assists22 = this.assists22;
+        assists21 = this.assists21;
+        tpg22 = this.tpg22;
+        tpg21 = this.tpg21;
+        trg22 = this.trg22;
+        trg21 = this.trg21;
+
+    }
+    
+
+}
