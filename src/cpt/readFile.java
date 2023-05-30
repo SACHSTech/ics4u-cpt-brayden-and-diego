7579bae1281cc73b3556aff5b98c7734a1818110
D1eg0Verdin <diego.verdinramirez23@ycdsbk12.ca>
2023-05-30 17:15:20 -0400

    created file reader

--- src/cpt/readFile.java
+++ src/cpt/readFile.java
@@ -1,5 +1,20 @@
 package cpt;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.*;
 
 public class readFile {
-    
+    public static team[] readDataFile(String strPath) {
+        //declaring variables
+        team[] finalList = new team[30];
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(strPath));
+        
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+    }
 }
