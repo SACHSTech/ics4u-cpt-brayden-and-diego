7226b478b7e669f794ce11ff92e6d9ea3013a523
D1eg0Verdin <diego.verdinramirez23@ycdsbk12.ca>
2023-06-01 12:54:57 -0400

    tested sorter, fixed small issue

--- src/cpt/Main.java
+++ src/cpt/Main.java
@@ -3,9 +3,17 @@ package cpt;
 public class Main {
     public static void main(String[] args) {
         Team[] a = readFile.readDataFile("/Users/diegoverdin/github-classroom/SACHSTech/ics4u-cpt-brayden-and-diego/src/cpt/Comp Sci CPT spreadsheet - Points Per Game.csv");
+        
+        Sortingpt2.sort(a,"ppg22");
+
         for (Team team : a) {
-            System.out.println(team.getTeamName());
+            System.out.println( " "  +team.getTeamName() + ": " + team.getPpg22());
+
         }
+
+        
+
+        
     }
     
 }
